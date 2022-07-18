59f2d0c736a638f0cf58725ae79e5bfff61c74b1
atainaive <atai.omorov@stud.vgtu.lt>
2022-07-18 17:32:10 +0300

    commited

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -1,11 +1,21 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
-    public void printHalfPyramid(int cathetusLength) {
-
-    }
-
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
+    }
+
+    public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int result = numberToPrint;
+        for (int i = 1; i < power; i++) {
+            result = result * numberToPrint;
+        }
 
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(0, 3);
     }
 }
